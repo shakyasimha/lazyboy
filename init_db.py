3748a02358267857f4b37ca6f486ac4c312d3023
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2024-12-16 09:11:51 +0545

    first commit after 5 days - working on argparse

--- init_db.py
+++ init_db.py
@@ -1,34 +1,43 @@
+"""
+    This is where the sqlite database is initialized and every sql query is run
+"""
+
 import sqlite3
 import os 
 
 DB_NAME = 'lazyboy.db'
 
 def init_db():
+    """Function for initializing sqlite
     """
-        Function for initializing sqlite3 upon running the application
-    """
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
-    
-    """
-        Now we run queries where we create a new table for the application
+    try: 
+        sqlite_connection = sqlite3.connect(DB_NAME)
+        cursor = sqlite_connection.cursor()
+        
+        """Now we run queries where we create a new table for the application
         
         Table: Tasks
         Columns:
-        - ID (auto generated)
-        - TASK_NAME (TEXT)
-        - AGE (INTEGER)
-        - URGENCY (INTEGER)
-        - COMPLETED (INTEGER)
-    """
-    cursor.execute('''
-        CREATE TABLE Task(
-            ID INTEGER PRIMARY KEY,
-            TASK_NAME TEXT,
-            AGE INTEGER, 
-            URGENCY INTEGER,
-            COMPLETED INTEGER
-        );
-    ''')
-    
+            ID (auto generated)
+            TASK_NAME (TEXT)
+            AGE (INTEGER)
+            URGENCY (INTEGER)
+            COMPLETED (INTEGER)
+        """
+        cursor.execute('''
+            CREATE TABLE Task(
+                ID INTEGER PRIMARY KEY,
+                TASK_NAME TEXT,
+                AGE INTEGER, 
+                URGENCY INTEGER,
+                COMPLETED INTEGER
+            );
+        ''')
+    except sqlite3.Error as error:
+        print('Error occured - ', error)
+    finally:
+        if sqlite3.Connection:
+            sqlite_connection.close()
+            print("Connecetion closed successfully")
+            
     
--- main.py
+++ main.py
@@ -1,24 +1,11 @@
-import sqlite3 
+import argparse 
 
-try:
-    sqliteConnection = sqlite3.connect('sql.db')
-    cursor = sqliteConnection.cursor()
-    print('DB init')
+def main():
+    """
+    Main driver function goes here
+    """
+    parser = argparse.ArgumentParser()
+    parser.parse_args()
     
-    # Writing a query to test it out
-    query = 'select sqlite_version();'
-    cursor.execute(query)
-    
-    # Fetching the result 
-    result = cursor.fetchall()
-    print(f'SQLite version is {result}')
-    
-    cursor.close()
-except sqlite3.Error as error:
-    print('Error occured - ', error)
-finally:
-    if sqlite3.Connection:
-        sqliteConnection.close()
-        print('SQLite connection closed')
-        
-        
+if __name__ == "__main__":
+    main()
