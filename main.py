f120dca212780bcd8f53cf289f314f8e203760c7
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2024-12-17 10:33:55 +0545

    working on parsing known arguments

--- main.py
+++ main.py
@@ -4,20 +4,39 @@ def main():
     """
     Main driver function goes here
     """
-    parser = argparse.ArgumentParser()
-    # parser.add_argument("square", type=int, help="square two numbers")
-    parser.add_argument("taskadd", help="Enter your task in the to-do list.")
-    parser.add_argument("-v", "--verbose", action="store_true", choices=[0,1,2], help="increase output verbosity")
-    # parser.add_argument("echo", help="echo the string you use in the command")
-    # parser.add_argument("square", help="squares two numbers", type=int)
-    args = parser.parse_args()
-    # answer = args.square**2
+    parser = argparse.ArgumentParser(prog='LazyBoy')
+    parser.add_argument('-n', '--name')
+    parser.add_argument('-v', '--verbose', action='store_true', default=0)
+
+    parser.add_argument("command", choices=["add", "remove", "list"], help="Add, remove or list the tasks")
+    args, remaining_args = parser.parse_known_args()
     
-    if args.verbose:
-        print(f"You have added the task: {args.taskadd}")
+    print(args)
+    print(remaining_args)
+    
+    if args.command == "args":
+        parser.add_argument("task", nargs="+", type=str)
+        args = parser.parse_args()
     else:
-        print(args.taskadd)
+        args = parser.parse_known_args(remaining_args)
+             
+    task_list = []
     
-    
+    match args.command:
+        case "add":
+            task_str = " ".join(args.task)
+            task_list.append(task_str)
+            
+            print(f"Task {task_list.index(task_str)+1} was added.")
+            if args.verbose: 
+                print(f"Task {task_list.index(task_str)+1} \'{task_str}\' was added.\n")
+        
+        case "list":
+            if not task_list: 
+                print("No task to show.")
+            else:
+                for t in task_list: 
+                    print(f"{task_list.index(t)+1} : {t} \n")
+                
 if __name__ == "__main__":
     main()
